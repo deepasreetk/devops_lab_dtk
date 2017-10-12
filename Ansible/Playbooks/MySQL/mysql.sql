4e35445ef2705c635948735df7cc8e3cf2085878
deepasreetk <31138200+deepasreetk@users.noreply.github.com>
2017-10-12 15:48:38 +0530

    Create mysql.sql
    Adding DB Table data in mysql.sql file...

---
+++ Ansible/Playbooks/MySQL/mysql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE STUDENTS_DEVOPS (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL
+);
+
+INSERT INTO STUDENTS_DEVOPS(firstname,lastname) VALUES('Student','1');
+INSERT INTO STUDENTS_DEVOPS(firstname,lastname) VALUES('Student','2');
